b6216d724f779aabf701d747e8d21349b24d16be
andre-hernandez03 <andre.hernandez03@tamu.edu>
2025-02-16 21:35:08 -0600

    Update API.py

--- API.py
+++ API.py
@@ -563,7 +563,7 @@ def ff_ccw(colors):
         colors['green'][i][2] = temp_white_row[i]
     colors['white'][0] = temp_blue_col[::-1]
     for i in range(3):
-        colors['blue'][i][0] = temp_yellow_row[i]
+        colors['blue'][i][2] = temp_yellow_row[i]
 
 
 def bf_ccw(colors):
